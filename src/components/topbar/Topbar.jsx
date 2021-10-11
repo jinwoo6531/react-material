9aaa377223920b422603496c593d19ae108afd2b
jinwoo <jintakk6531@gmail.com>
2021-10-11 20:55:02 +0900

    10/11

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,8 +5,10 @@ const Topbar = () => {
     return (
         <div className="topbar">
             <div className="topbarWrapper">
+
                 <div className="topLeft">left</div>
                 <div className="topRight">right</div>
+                
             </div>
         </div>
     )
