82c19030bfbc5fecab149d811e69ee82f4018fe3
jinwoo <jintakk6531@gmail.com>
2021-10-12 21:06:39 +0900

    10/12

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import "./topbar.css";
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
+
 
 const Topbar = () => {
     return (
         <div className="topbar">
             <div className="topbarWrapper">
 
-                <div className="topLeft">left</div>
-                <div className="topRight">right</div>
-                
+                <div className="topLeft">
+                    <span className="logo">lamaadmin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
